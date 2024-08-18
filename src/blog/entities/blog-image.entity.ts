324baaa440fd5b511c175049da2573f83093481d
Sanuth23 <sanuth0000@gmail.com>
2024-08-18 20:59:40 +0530

    create blog & blog image entities & mapping them

--- src/blog/entities/blog-image.entity.ts
+++ src/blog/entities/blog-image.entity.ts
@@ -1,1 +1,18 @@
-export class BlogImage {}
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Blog } from "./blog.entity";
+
+@Entity()
+export class BlogImage {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    path: string;
+
+    @Column()
+    blogId: number;
+
+    @ManyToOne(() => Blog, (blog) => blog.blogImages)
+    @JoinColumn({ name: 'blogId' })
+    blogs: Blog;
+}
--- src/blog/entities/blog.entity.ts
+++ src/blog/entities/blog.entity.ts
@@ -1,1 +1,45 @@
-export class Blog {}
+import { Exclude } from "class-transformer";
+import { Column, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { BlogImage } from "./blog-image.entity";
+
+@Entity()
+export class Blog {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    topic: string;
+
+    @Column()
+    description: string;
+
+    @Column()
+    adminId: number;
+
+
+    @Column()
+    createdBy: number;
+
+    @Column()
+    updatedBy: number;
+
+    @Column()
+    deletedBy: number;
+
+    @Column()
+    createdAt: Date;
+
+    @Column()
+    updatedAt: Date;
+
+    @Column()
+    deletedAt: Date;
+
+    @Exclude()
+    @OneToMany(() => BlogImage, (blogImage) => blogImage.blogs)
+    blogImages: BlogImage[];
+
+    // @ManyToOne(() => Admin, (admin) => admin.blog)
+    // @JoinColumn({name:'adminId'})
+    // admin: Admin;
+}

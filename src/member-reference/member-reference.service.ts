6f1b25a81d038d4d5f662b53d6c48ade2e721761
Sanuth23 <sanuth0000@gmail.com>
2024-08-26 12:35:57 +0530

    implement create , findAll, findByMemberId, findByCategoryId apis in member reference

--- src/member-reference/member-reference.service.ts
+++ src/member-reference/member-reference.service.ts
@@ -1,26 +1,117 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateMemberReferenceDto } from './dto/create-member-reference.dto';
 import { UpdateMemberReferenceDto } from './dto/update-member-reference.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { MemberReference } from './entities/member-reference.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class MemberReferenceService {
-  create(createMemberReferenceDto: CreateMemberReferenceDto) {
-    return 'This action adds a new memberReference';
+  constructor(
+    @InjectRepository(MemberReference) private readonly referenceRepository: Repository<MemberReference>
+  ) { }
+  
+  async create(createMemberReferenceDto: CreateMemberReferenceDto) {
+    if (createMemberReferenceDto.teamMemberId == null && createMemberReferenceDto.productCategoryId == null) {
+      throw new BadRequestException('When creating a member reference, member id & category id is a required field.');
+    }
+
+    try {
+      createMemberReferenceDto.createdAt = new Date();
+      const reference = this.referenceRepository.create(createMemberReferenceDto);
+      const referenceEntity = await this.referenceRepository.save(reference);;
+      return referenceEntity != null ? referenceEntity : "Can't save the reference. Please check the details & try again.";
+    } catch (error) {
+      throw new InternalServerErrorException('Failed to create member reference', error.message);
+    }
   }
 
-  findAll() {
-    return `This action returns all memberReference`;
+  async findAll() {
+    try {
+      const allReferences: MemberReference[] = await this.referenceRepository.find({
+        relations: [],
+      });
+
+      if (allReferences) {
+        let finalizeReferences: MemberReference[] = allReferences.filter((element) => {
+          if (element.deletedBy == null && element.teamMemberId != null && element.productCategoryId != null) {
+            return (element)
+          }
+        });
+
+        finalizeReferences.sort((a1, a2) =>
+          a2.createdAt.getTime() - a1.createdAt.getTime()
+        );
+
+        return finalizeReferences.length == 0 ? 'No Member Reference found.' : finalizeReferences;
+      }
+      return 'No Member Reference found.';
+    } catch (error) {
+      throw new InternalServerErrorException('Failed to retrieve member reference', error.message);
+    }
   }
 
-  findOne(id: number) {
+  async findByMemberId(memberId: number) {
+    try {
+      const allReferences: MemberReference[] = await this.referenceRepository.find({
+        where: { teamMemberId: memberId },
+        relations: [],
+      });
+
+      if (allReferences) {
+        let finalizeReferences: MemberReference[] = allReferences.filter((element) => {
+          if (element.deletedBy == null && element.productCategoryId != null) {
+            return (element)
+          }
+        });
+
+        finalizeReferences.sort((a1, a2) =>
+          a2.createdAt.getTime() - a1.createdAt.getTime()
+        );
+
+        return finalizeReferences.length == 0 ? 'No Member Reference found.' : finalizeReferences;
+      }
+      return 'No Member Reference found.';
+    } catch (error) {
+      throw new InternalServerErrorException('Failed to retrieve member reference', error.message);
+    }
+  }
+
+  async findByCategoryId(categoryId: number) {
+    try {
+      const allReferences: MemberReference[] = await this.referenceRepository.find({
+        where: { productCategoryId: categoryId },
+        relations: [],
+      });
+
+      if (allReferences) {
+        let finalizeReferences: MemberReference[] = allReferences.filter((element) => {
+          if (element.deletedBy == null && element.teamMemberId != null) {
+            return (element)
+          }
+        });
+
+        finalizeReferences.sort((a1, a2) =>
+          a2.createdAt.getTime() - a1.createdAt.getTime()
+        );
+
+        return finalizeReferences.length == 0 ? 'No Member Reference found.' : finalizeReferences;
+      }
+      return 'No Member Reference found.';
+    } catch (error) {
+      throw new InternalServerErrorException('Failed to retrieve member reference', error.message);
+    }
+  }
+
+  async findOne(id: number) {
     return `This action returns a #${id} memberReference`;
   }
 
-  update(id: number, updateMemberReferenceDto: UpdateMemberReferenceDto) {
+  async update(id: number, updateMemberReferenceDto: UpdateMemberReferenceDto) {
     return `This action updates a #${id} memberReference`;
   }
 
-  remove(id: number) {
+  async remove(id: number) {
     return `This action removes a #${id} memberReference`;
   }
 }

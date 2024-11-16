e4fbab088409364b8331e7a9b9620bfd6c154dff
Sanuth23 <sanuth0000@gmail.com>
2024-11-16 21:44:45 +0530

    add new packages

--- src/contact-us/contact-us.module.ts
+++ src/contact-us/contact-us.module.ts
@@ -3,10 +3,11 @@ import { ContactUsService } from './contact-us.service';
 import { ContactUsController } from './contact-us.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ContactUs } from './entities/contact-us.entity';
+import { EmailService } from 'src/email/email.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ContactUs])],
   controllers: [ContactUsController],
-  providers: [ContactUsService],
+  providers: [ContactUsService,EmailService],
 })
 export class ContactUsModule {}
